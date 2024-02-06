9c13e30d2f07856b78f6f360eb312d8fc5d8c03b
Sai Swayam <saiswayamshree19@gmail.com>
2024-02-06 20:15:48 +0530

    Fixed layers button issue

--- NeuralNetwork/main.js
+++ NeuralNetwork/main.js
@@ -14,6 +14,24 @@ add.addEventListener("click", () => {
     let layer = document.createElement("div");
     layer.className = "layer";
     layer.id = `${i}`;
+    layer.innerHTML = `
+      <div class="button_container">
+        <button class="btn-s">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+            stroke="currentColor" width="1.5rem">
+            <path stroke-linecap="round" stroke-linejoin="round"
+              d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+          </svg>
+
+        </button>
+        <button class="btn-s">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+            stroke="currentColor" width="1.5rem">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M15 12H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+          </svg>
+        </button>
+      </div>
+    `;
     graph.appendChild(layer);
     layers.push(layer);
   }
